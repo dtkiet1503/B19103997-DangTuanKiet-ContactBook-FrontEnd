04494e28d740579f4406676f44b814ea68c19f9e
dtkiet1503 <dangtuankiet1503@gmail.com>
2023-03-28 16:59:53 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+const routes = [
+    {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true
+    },
+    ];
+    const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+    });
+    
+export default router;
